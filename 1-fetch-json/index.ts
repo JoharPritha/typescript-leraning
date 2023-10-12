9329fb08df9e2ade9e106e6b814f549162ec9811
Pritha Johar <prithajohar@gmail.com>
2023-10-12 13:10:30 +0530

    basic api call with data type define by interface

--- 1-fetch-json/index.ts
+++ 1-fetch-json/index.ts
@@ -2,6 +2,27 @@ import axios from 'axios';
 
 const url='https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo{
+    id:number;
+    title:string;
+    completed:boolean;
+}
+
 axios.get(url).then(response=>{
-    console.log(response.data);
+    //response.data has property of :
+    //id
+    //title
+    //completed
+
+    const todo=response.data as Todo;
+
+    const ID=todo.id;
+    const title=todo.title;
+    const completed=todo.completed;
+
+    console.log(`
+        The todo with ID : ${ID} 
+        Has a title of : ${title}
+        is it completed? ${completed}
+    `)
 })
